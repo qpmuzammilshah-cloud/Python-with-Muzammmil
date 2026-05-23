4f57f7c8845cac07ccf1aebb4f6aeef0279a8661
QP Muzammil Shah <thinkwithmuzammil@gmail.com>
2026-05-22 23:12:41 -0700

    Add basic calculator functionality in calculator.py
    Implement basic calculator functionality for addition, subtraction, multiplication, and division.

---
+++ 01-calculator/calculator.py
@@ -0,0 +1,12 @@
+num1=float(input("enter the first number"))
+num2=float(input("enter the second number"))
+user_choice=input("choose the opration /, -, + ,*")
+if user_choice=="+":
+  print(num1+num2)
+elif user_choice=="-":
+  print(num1-num2)
+elif user_choice=="*":
+  print(num1*num2)
+elif user_choice=="/":
+  print(num1/num2)
+  #note this is my first python program   
